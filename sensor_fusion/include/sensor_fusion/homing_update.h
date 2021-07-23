7b7a5dbf8d15b0f50931c640b5c26f0f7560b6e8
jsngross <Jason.Gross@mail.wvu.edu>
2021-07-23 13:50:53 -0400

    new service to perform homing wrt processing plant

--- sensor_fusion/include/sensor_fusion/homing_update.h
+++ sensor_fusion/include/sensor_fusion/homing_update.h
@@ -11,8 +11,10 @@
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Point.h>
 #include <sensor_fusion/HomingUpdate.h>
+#include <sensor_fusion/HomingUpdateProcessingPlant.h>
 #include <sensor_fusion/SetBaseLocation.h>
 #include <range_to_base/LocationOfBase.h>
+#include <range_to_base/LocationOfProcessingPlant.h>
 
 // ROS headers
 #include <ros/ros.h>
@@ -33,18 +35,24 @@ private:
 	ros::Publisher pubBaseLocation_;
 	ros::Subscriber subBaseLocation_;
 	ros::ServiceServer homingUpdateServer_;
+	ros::ServiceServer homingUpdateProcessingPlantServer_;
 	ros::ServiceServer setBaseLocationServer_;
 
 	ros::ServiceClient baseLocationClient_;
+	ros::ServiceClient processingPlantLocationClient_;
 
 	geometry_msgs::Point baseStationLocation_;
 	geometry_msgs::Pose measurementUpdate_;
+	geometry_msgs::Point processingPlantLocation_;
+	geometry_msgs::Pose measurementUpdateProcessingPlant_;
 
 
 	bool homingUpdate_(sensor_fusion::HomingUpdate::Request &req, sensor_fusion::HomingUpdate::Response &res);
+	bool homingUpdateProcessingPlant_(sensor_fusion::HomingUpdateProcessingPlant::Request &req, sensor_fusion::HomingUpdateProcessingPlant::Response &res);
 	bool setBaseLocation_(sensor_fusion::SetBaseLocation::Request &req, sensor_fusion::SetBaseLocation::Response &res);
 
 	void baseLocationCallback_(const geometry_msgs::Point::ConstPtr& msg);
+	void processingPlantLocationCallback_(const geometry_msgs::Point::ConstPtr& msg);
 
 };
 
--- sensor_fusion/src/homing_update.cpp
+++ sensor_fusion/src/homing_update.cpp
@@ -6,11 +6,15 @@ HomingUpdate::HomingUpdate(ros::NodeHandle & nh)
 	firstCallback_=true;
   std::string node_name = "homing_update";
 	baseLocationClient_ = nh_.serviceClient<range_to_base::LocationOfBase>("location_of_base_service");
+	processingPlantLocationClient_ = nh_.serviceClient<range_to_base::LocationOfProcessingPlant>("location_of_processing_plant_service");
+
 	homingUpdateServer_ = nh_.advertiseService("homing",&HomingUpdate::homingUpdate_,this);
+  homingUpdateProcessingPlantServer_ = nh_.advertiseService("homing_processing_plant",&HomingUpdate::homingUpdateProcessingPlant_,this);
 	setBaseLocationServer_ = nh_.advertiseService("set_base_location",&HomingUpdate::setBaseLocation_,this);
 
 	pubMeasurementUpdate_ = nh_.advertise<geometry_msgs::Pose>("position_update",10);
 	pubBaseLocation_ = nh_.advertise<geometry_msgs::Point>("/base_station",10);
+
 
 	subBaseLocation_ = nh_.subscribe(
 			"/base_station", 1, &HomingUpdate::baseLocationCallback_, this);
@@ -31,6 +35,24 @@ HomingUpdate::HomingUpdate(ros::NodeHandle & nh)
 		baseStationLocation_.x = base_x;
 		baseStationLocation_.y = base_y;
 		ROS_INFO(" Saving Base Station as Landmark x:%f y:%f", base_y, base_y);
+
+   double processing_plant_x, processing_plant_y;
+		if (ros::param::get(node_name + "/processing_plant_x", processing_plant_x ) ==
+			  false) {
+			  ROS_FATAL("No parameter 'processing_plant_x' specified");
+			  ros::shutdown();
+			  exit(1);
+		 }
+
+		if (ros::param::get(node_name + "/processing_plant_y", processing_plant_y ) ==
+					false) {
+				ROS_FATAL("No parameter 'processing_plant_y' specified");
+				ros::shutdown();
+				exit(1);
+			}
+			processingPlantLocation_.x = processing_plant_x;
+			processingPlantLocation_.y = processing_plant_y;
+
 }
 void HomingUpdate::baseLocationCallback_(const geometry_msgs::Point::ConstPtr& msg){
 
@@ -96,6 +118,44 @@ bool HomingUpdate::homingUpdate_(sensor_fusion::HomingUpdate::Request &req, sens
 
 	//also returning the estimate of the base station location
 	res.base_location = baseStationLocation_;
+
+	return true;
+}
+
+
+
+bool HomingUpdate::homingUpdateProcessingPlant_(sensor_fusion::HomingUpdateProcessingPlant::Request &req, sensor_fusion::HomingUpdateProcessingPlant::Response &res){
+
+
+	range_to_base::LocationOfProcessingPlant srv;
+
+	srv.request.angle = req.angle;
+	while (!processingPlantLocationClient_.waitForExistence())
+	{
+			ROS_WARN("HOMING: Waiting for Location of Processing Plant Location Service");
+	}
+	processingPlantLocationClient_.call(srv);
+
+
+
+		if(fabs(srv.response.position.x) < 100.0 && fabs(srv.response.position.y) < 100)
+		{
+			// our measurement is the relative error in x and y
+			measurementUpdate_.position.x=processingPlantLocation_.x - srv.response.position.x;
+			measurementUpdate_.position.y=processingPlantLocation_.y - srv.response.position.y;
+			measurementUpdate_.position.z=processingPlantLocation_.z - 0.0; // z isnt actually used...
+			pubMeasurementUpdate_.publish(measurementUpdate_);
+			res.success =true;
+		}
+		else
+		{
+			res.success = false;
+		}
+
+
+
+	//also returning the estimate of the base station location
+	res.processing_plant_location = processingPlantLocation_;
 
 	return true;
 }
